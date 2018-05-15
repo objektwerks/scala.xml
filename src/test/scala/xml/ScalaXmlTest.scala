2fe2a7f19f48f357cf4448c585117cc1d3b6c2e5
objektwerks <objektwerks@runbox.com>
2018-05-15 08:16:14 -0700

    added todo xml and xsd; built scala xml validate test

--- src/test/scala/xml/ScalaXmlTest.scala
+++ src/test/scala/xml/ScalaXmlTest.scala
@@ -1,8 +1,20 @@
 package xml
 
+import java.io.StringReader
+
+import javax.xml.transform.stream.StreamSource
+import javax.xml.validation.SchemaFactory
 import org.scalatest.{FunSuite, Matchers}
 
+import scala.xml.XML
+
 class ScalaXmlTest extends FunSuite with Matchers {
-  test("xml") {
+  test("validate") {
+    val factory = SchemaFactory.newInstance("http://www.w3.org/2001/XMLSchema")
+    val schema = factory.newSchema(getClass.getResource("/todo.xsd"))
+    val validator = schema.newValidator()
+    val xml = XML.load(getClass.getResource("/todo.xml")).toString
+    println(xml)
+    validator.validate(new StreamSource(new StringReader(xml)))
   }
 }
