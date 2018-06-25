706c107bb6fafc5ec1737c4f33aafbbbc2a357f0
objektwerks <objektwerks@runbox.com>
2018-06-25 08:32:57 -0700

    Update Todos.scala
    Added implicits.

--- src/test/scala/xml/Todos.scala
+++ src/test/scala/xml/Todos.scala
@@ -3,26 +3,33 @@ package xml
 import scala.xml.Node
 
 case class Name(value: String)
+
 case class Task(value: String)
+
 case class Todo(name: Name, task: Task)
+
 case class Todos(values: Seq[Todo])
 
 object Todos extends ScalaXmlSupport {
-  def fromXml(todos: Node): Todos = Todos {
-    (todos \\ "todo").map { todo =>
-      val name = Name((todo \\ "name").text)
-      val task = Task((todo \\ "task").text)
-      Todo(name, task)
+  implicit class NodeOps(val todos: Node) {
+    def toTodos: Todos = Todos {
+      (todos \\ "todo").map { todo =>
+        val name = Name((todo \\ "name").text)
+        val task = Task((todo \\ "task").text)
+        Todo(name, task)
+      }
     }
   }
 
-  def toXml(todos: Todos): Node =
-    <todos>
-      {todos.values.map { todo =>
-      <todo>
-        <name>{todo.name.value}</name>
-        <task>{todo.task.value}</task>
-      </todo>
-    }}
-    </todos>
+  implicit class TodosOps(val todos: Todos) {
+    def toXml: Node =
+      <todos>
+        {todos.values.map { todo =>
+        <todo>
+          <name>{todo.name.value}</name>
+          <task>{todo.task.value}</task>
+        </todo>
+      }}
+      </todos>
+  }
 }
