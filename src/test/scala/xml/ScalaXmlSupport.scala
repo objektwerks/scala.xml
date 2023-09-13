fd86bcc4ad3e2847a6f5ef2641d4fe822bd9d99a
objektwerks <objektwerks@runbox.com>
2023-09-13 13:33:14 -0400

    reformatted scala xml support

--- src/test/scala/xml/ScalaXmlSupport.scala
+++ src/test/scala/xml/ScalaXmlSupport.scala
@@ -10,7 +10,7 @@ import scala.io.Source
 import scala.util.Try
 import scala.xml.{Node, NodeSeq, PrettyPrinter, XML}
 
-trait ScalaXmlSupport {
+trait ScalaXmlSupport:
   def loadXml(xmlClasspath: String): Try[Node] = Try {
     XML.load(getClass.getResource(xmlClasspath))
   }
@@ -20,12 +20,12 @@ trait ScalaXmlSupport {
   }
 
   def loadValidatedXml(xsdClasspath: String, xmlClasspath: String): Try[Node] =
-    for {
+    for
       schema <- loadSchema(xsdClasspath)
-      xml <- loadRawXml(xmlClasspath)
-      _ = schema.newValidator.validate(new StreamSource(new StringReader(xml)))
-      elem = XML.loadString(xml)
-    } yield elem
+      xml    <- loadRawXml(xmlClasspath)
+      _      = schema.newValidator.validate(new StreamSource(new StringReader(xml)))
+      elem   = XML.loadString(xml)
+    yield elem
 
   def loadSchema(xsdClasspath: String): Try[Schema] = Try {
     SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI).newSchema(getClass.getResource(xsdClasspath))
@@ -35,16 +35,13 @@ trait ScalaXmlSupport {
     XML.save(fileName, node, "UTF-8", xmlDecl = true, doctype = null)
   }
 
-  def formatXml(nodeSeq: NodeSeq): String = {
+  def formatXml(nodeSeq: NodeSeq): String =
     val printer = new PrettyPrinter(width = 120, step = 2)
     nodeSeq.map(node => printer.format(node))
     nodeSeq.toString
-  }
 
-  def isXmlValid(schema: Schema, node: Node): Boolean = try {
-    schema.newValidator.validate(new StreamSource(new StringReader(node.toString)))
-    true
-  } catch {
-    case _: Throwable => false
-  }
-}
+  def isXmlValid(schema: Schema, node: Node): Boolean =
+    try
+      schema.newValidator.validate(new StreamSource(new StringReader(node.toString)))
+      true
+    catch case _: Throwable => false
