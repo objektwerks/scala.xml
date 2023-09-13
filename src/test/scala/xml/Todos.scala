1e038a6b4783e0a3b04141705d7d15f65280360c
objektwerks <objektwerks@runbox.com>
2023-09-13 13:35:52 -0400

    reformatted todos

--- src/test/scala/xml/Todos.scala
+++ src/test/scala/xml/Todos.scala
@@ -2,16 +2,16 @@ package xml
 
 import scala.xml.Node
 
-case class Name(value: String)
+final case class Name(value: String)
 
-case class Task(value: String)
+final case class Task(value: String)
 
-case class Todo(name: Name, task: Task)
+final case class Todo(name: Name, task: Task)
 
-case class Todos(values: Seq[Todo])
+final case class Todos(values: Seq[Todo])
 
-object Todos extends ScalaXmlSupport {
-  implicit class NodeOps(val todos: Node) {
+object Todos extends ScalaXmlSupport:
+  implicit final class NodeOps(val todos: Node):
     def toTodos: Todos = Todos {
       (todos \\ "todo").map { todo =>
         val name = Name((todo \\ "name").text)
@@ -19,9 +19,8 @@ object Todos extends ScalaXmlSupport {
         Todo(name, task)
       }
     }
-  }
 
-  implicit class TodosOps(val todos: Todos) {
+  implicit final class TodosOps(val todos: Todos):
     def toXml: Node =
       <todos>
         {todos.values.map { todo =>
@@ -31,5 +30,3 @@ object Todos extends ScalaXmlSupport {
         </todo>
       }}
       </todos>
-  }
-}
